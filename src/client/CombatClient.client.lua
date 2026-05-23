12eba4460291d1a8f84220e03d955b73fd4a4694
Ifeoye05 <157320406+Ifeoye05@users.noreply.github.com>
2026-05-23 20:58:12 +0200

    Added cooldown to combatclient m1s

--- src/client/CombatClient.client.lua
+++ src/client/CombatClient.client.lua
@@ -1,9 +1,14 @@
 local UserInputService = game:GetService("UserInputService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local PunchEvent = ReplicatedStorage:WaitForChild("PunchEvent")
+local CanPunch = true
 UserInputService.InputBegan:Connect(function(input, gameProcessed)
     if gameProcessed then return end
     if input.UserInputType == Enum.UserInputType.MouseButton1 then
+        if CanPunch == false then return end
         PunchEvent:FireServer()
+        CanPunch = false
+        task.wait(0.5)
+        CanPunch = true
     end
 end)
