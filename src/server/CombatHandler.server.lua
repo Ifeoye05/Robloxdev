165b7b15ca18219b73d3592024f40b5ecdf2824b
Ifeoye05 <157320406+Ifeoye05@users.noreply.github.com>
2026-05-23 19:22:11 +0200

    Scripted CombatHandler server side

---
+++ src/server/CombatHandler.server.lua
@@ -0,0 +1,18 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local PunchEvent = ReplicatedStorage:WaitForChild("PunchEvent")
+PunchEvent.OnServerEvent:Connect(function(player)
+    local Character = player.Character
+    local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+    local Characters = game.Workspace:GetChildren()
+    for _, otherCharacter in ipairs(Characters) do
+        if Character == otherCharacter then continue end
+        local humanoid = otherCharacter:FindFirstChild("Humanoid")
+        if humanoid == nil then continue end
+        local otherHumanoidRootPart = otherCharacter:FindFirstChild("HumanoidRootPart")
+        if otherHumanoidRootPart == nil then continue end
+
+        if (HumanoidRootPart.Position - otherHumanoidRootPart.Position).Magnitude <= 7 then
+            humanoid:TakeDamage(5)
+        end
+    end
+end)
