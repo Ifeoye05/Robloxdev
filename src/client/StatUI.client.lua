2f3d23a4318664391843fb7d0f33d5b7c49cde7f
Ifeoye05 <iadare2005@gmail.com>
2026-07-07 00:56:30 +0200

    Refactor StatUI to properly update player stats on event trigger(StatUI not complete)

--- src/client/StatUI.client.lua
+++ src/client/StatUI.client.lua
@@ -1,4 +1,15 @@
 local statupdateEvent = game:GetService("ReplicatedStorage"):WaitForChild("StatUpdateEvent")
+local plr = game:GetService("Players").LocalPlayer
+local StatValues = plr:WaitForChild("PlayerGui"):WaitForChild("StatGui"):WaitForChild("Mainframe"):WaitForChild("Secondaryframe"):WaitForChild("Statvalues")
+local Strength = StatValues:WaitForChild("StrengthValue")
+local Defense = StatValues:WaitForChild("DefenseValue")
+local Special = StatValues:WaitForChild("SpecialValue")
+local Statpoints = StatValues:WaitForChild("StatpointsValue")
 
 statupdateEvent.OnClientEvent:Connect(function(statdata)
-    
+    Strength.Text = statdata.Strength
+    Defense.Text = statdata.Defense
+    Special.Text = statdata.Special
+    Statpoints.Text = statdata.StatPoints
+
+end)
