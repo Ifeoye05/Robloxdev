fe21a516858c36ccb4f79010eaf5c53bfecfb31c
Ifeoye05 <iadare2005@gmail.com>
2026-07-07 20:50:19 +0200

    Refactor DamageModule to use targethumanoid parameter directly for damage calculations

--- src/shared/DamageModule.lua
+++ src/shared/DamageModule.lua
@@ -2,16 +2,14 @@ local DamageModule = {}
 local statModule = require(game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("StatModule"))
 
 
-function DamageModule.dealregularDamage(plr, target)
+function DamageModule.dealregularDamage(plr, targethumanoid)
     local plrStrength = statModule.getStat(plr, "Strength")
-    local targetHumanoid = target.Character.Humanoid
-    targetHumanoid:TakeDamage(5+(plrStrength*2))
+    targethumanoid:TakeDamage(5+(plrStrength*2))
 end
 
-function DamageModule.dealspecialDamage(plr, target)
+function DamageModule.dealspecialDamage(plr, targethumanoid)
     local plrSpecial = statModule.getStat(plr, "Special")
-    local targetHumanoid = target.Character.Humanoid
-    targetHumanoid:TakeDamage(10+(plrSpecial*5))
+    targethumanoid:TakeDamage(10+(plrSpecial*5))
 end
 
 
