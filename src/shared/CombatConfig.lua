92ac125384e806b48f337324624347e7bef0968e
Ifeoye05 <iadare2005@gmail.com>
2026-07-08 01:23:24 +0200

    Implement character stat update on respawn and adjust PunchStun value in CombatConfig

--- src/shared/CombatConfig.lua
+++ src/shared/CombatConfig.lua
@@ -1,7 +1,7 @@
 local CombatConfig = {
     Damage = 5,
     PunchCD = 0.2,
-    PunchStun = 0.3,
+    PunchStun = 0.1,
     BlockReduction = 0, -- Damage is multiplied by the block reduction so 0 means 1 -- 
     FireballDamage = 30,
     FireballCD = 2,
